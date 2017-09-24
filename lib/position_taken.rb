618cc6238914395800eae8b72994a6b509400186
Walter Minehuber <minehuber@gmail.com>
2017-09-24 13:28:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, check)
-  if board[check]!=" " || board[check]!=""
+  if board[check]==" " || board[check]==""
     false
   elsif board[check]=="X" || board[check]=="O"
     true
